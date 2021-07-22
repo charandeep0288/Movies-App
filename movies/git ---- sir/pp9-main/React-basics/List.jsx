6330d8d3d39efb5a64d65aaf8afdfaa96d0f8d43
Charandeep Singh <67065286+charandeep0288@users.noreply.github.com>
2021-07-22 18:24:10 +0530

    Delete movies/git ---- sir directory

--- movies/git ---- sir/pp9-main/React-basics/List.jsx
+++
@@ -1,9 +0,0 @@
-function List(props) {
-  return (
-    <ul>
-      {props.tasks.map((el,index) => {
-        return <ListItem removeTask = {props.removeTaskHandler}  key={index}  taskVal={el} />;
-      })}
-    </ul>
-  );
-}
--- movies/git ---- sir/pp9-main/movie-api/readme.md
+++
@@ -1,9 +0,0 @@
-# Steps 
-- new folder with name using small letters and no spaces 
-- npm init -y (creates a package.json file)
-- npm i express (installs the express framework)
-
-
-
-
-
